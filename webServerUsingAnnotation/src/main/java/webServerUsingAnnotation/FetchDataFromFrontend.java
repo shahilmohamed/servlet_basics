9f9d1d3ef80dd205e1fed675f4e72175c3d2b032
Ashik <Ashik@Shahil>
2024-02-07 23:31:22 +0530

    First commit

---
+++ webServerUsingAnnotation/src/main/java/webServerUsingAnnotation/FetchDataFromFrontend.java
@@ -0,0 +1,43 @@
+package webServerUsingAnnotation;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/fetchdata")
+public class FetchDataFromFrontend extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		String id = req.getParameter("id");
+		String name = req.getParameter("name");
+		String age = req.getParameter("age");
+		String dept = req.getParameter("dept");
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/college", "root", "12345@Shahil");
+			PreparedStatement ps = con.prepareStatement("insert into student(id,name,age,dept) values(?,?,?,?)");
+			ps.setInt(1, Integer.parseInt(id));
+			ps.setString(2, name);
+			ps.setInt(3, Integer.parseInt(age));
+			ps.setString(4, dept);
+			ps.execute();
+			System.out.println("Data inserted in database");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
